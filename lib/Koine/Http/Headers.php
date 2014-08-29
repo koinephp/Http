ce0f4d32803e58ac76acb35069680008843cc16c
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2014-08-29 17:42:01 -0300

    WIP on the Headers

---
+++ lib/Koine/Http/Headers.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Koine\Http;
+
+use Koine\Hash;
+
+/**
+ * @author Marcelo Jacobus <marcelo.jacobus@gmail.com>
+ */
+class Headers extends Hash
+{
+    /**
+     * @var boolean
+     */
+    protected $sent = false;
+
+    /**
+     * Checks if the headers have been sent
+     * @return boolean
+     */
+    public function sent()
+    {
+        return $this->sent;
+    }
+
+    /**
+     * Send the headers
+     * @return self
+     */
+    public function send()
+    {
+        if ($this->sent()) {
+            // TODO: throw exception
+        }
+
+        $this->sent = true;
+
+        foreach ($this as $header => $value) {
+            header("$header: $value");
+        }
+
+        return $this;
+    }
+
+    /**
+     * Set the location to redirect to
+     *
+     * @param string $contentType
+     * @return self
+     */
+    public function setLocation($location)
+    {
+        $this['Location'] = $location;
+
+        return $this;
+    }
+
+    /**
+     * Set the content type
+     *
+     * @param string $contentType
+     * @return self
+     */
+    public function setContentType($contentType)
+    {
+        $this['Content-Type'] = $contentType;
+
+        return $this;
+    }
+}
---
+++ tests/KoineTests/Http/HeadersTest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace KoineTess;
+
+use Koine\Http\Headers;
+use PHPUnit_Framework_TestCase;
+
+/**
+ * @author Marcelo Jacobus <marcelo.jacobus@gmail.com>
+ */
+class HeadersTest extends PHPUnit_Framework_TestCase
+{
+    protected $object;
+
+    public function setUp()
+    {
+        $this->object = new Headers();
+    }
+
+    /**
+     * @test
+     */
+    public function extendsHash()
+    {
+        $this->assertInstanceOf('Koine\Hash', $this->object);
+    }
+
+    /**
+     * @test
+     */
+    public function isInitiallyNotSent()
+    {
+        $this->assertFalse($this->object->sent());
+    }
+
+    /**
+     * @test
+     * @runInSeparateProcess
+     */
+    public function sendSendsHeader()
+    {
+        $this->object['Location'] = '/';
+        $this->assertTrue($this->object->send()->sent());
+
+        // Won't return the correct headers
+        // $this->assertEquals(array('Location: /'), headers_list());
+    }
+
+    /**
+     * @test
+     */
+    public function setLocationSetsTheHeaderLocation()
+    {
+        $headers = $this->object->setLocation('/')->toArray();
+
+        $this->assertEquals(array('Location' => '/'), $headers);
+    }
+
+    /**
+     * @test
+     */
+    public function setContentTypeSetsTheContentType()
+    {
+        $headers = $this->object->setContentType('application/json')->toArray();
+
+        $expected = array('Content-Type' => 'application/json');
+
+        $this->assertEquals($expected, $headers);
+    }
+}
